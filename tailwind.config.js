16c8f12fa40b82abc945a1977862f40db617d022
GFFritz <gabrielf.fritz@gmail.com>
2024-02-18 17:59:49 -0300

    layout settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,7 @@ module.exports = {
   ],
   theme: {
     fontFamily: {
-      sans: ['DM Sans', 'sans-serif'],
-      'montserrat': ['Montserrat', 'sans-serif']
+      sans: ['Segoe Ui', 'sans-serif']
     },
     extend: {
       container: {
@@ -17,10 +16,20 @@ module.exports = {
         padding: '0',
         screens: {
           xs: '419px',
-          md: '640px',
-          lg: '1211px',
+          md: '419px',
+          lg: '419px',
+          xl: '419px',
+          '2xl': '419px',
+          '3xl': '419px'
         },
       },
+      colors: {
+        primary: '#1B3A68',
+        'primary-alt': '#1c4c8c'
+      },
+      boxShadow: {
+        'button': '0px 3px 3px 0px #00000029',
+      }
     },
   },
   plugins: [],
